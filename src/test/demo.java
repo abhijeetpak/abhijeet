11eb7b0756b54585c4b9ab8b8f3ec951c8ca0a66
Abhijeet <Abhijeet@DESKTOP-VK4PF7E>
2022-11-19 22:13:41 +0530

    hiiii

---
+++ src/test/demo.java
@@ -0,0 +1,40 @@
+package test;
+
+public class demo {
+
+	
+	
+
+		public static void main(String[] args) {
+			
+			System.out.println("hiii");
+			m1();
+		}
+		
+		
+		
+		public static void  m1() {
+			
+			
+			System.out.println("abhijeet pakale");
+			
+			
+		}
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
